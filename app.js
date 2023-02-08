29f0d4488ffbc2ba92486a2de32aabefe848bfb6
“Niek” <n.veenstra02@gmail.com>
2023-02-08 16:16:49 +0100

    finished js logic for bank

---
+++ app.js
@@ -0,0 +1,41 @@
+const getLoan = document.getElementById("getLoanButton");
+const bankBalance = document.getElementById("bankBalance");
+
+let balance = 2069;
+let loanBalance = 0;
+let totalBalance = 0;
+
+bankBalance.innerText = balance;
+
+const handleGetLoan = () => {
+  if (loanBalance !== 0) {
+    console.log(
+      "You may not have two loans at once. The initial loan should be paid back in full."
+    );
+    return;
+  }
+
+  const loanInput = prompt("Please enter the amount you want to loan:", "100");
+  const loanBalanceInput = parseInt(loanInput);
+
+  if (isNaN(loanBalanceInput)) {
+    console.log("Please give a valid number");
+    return;
+  }
+
+  if (loanBalanceInput >= 2 * balance) {
+    console.log("You cannot get a loan more than double of your bank balance");
+    return;
+  }
+
+  loanBalance += loanBalanceInput;
+
+  const table = document.createElement("TD");
+  const tableName = document.createTextNode("Outstanding loan");
+  table.appendChild(tableName);
+  const tableValue = document.createTextNode(loanBalance);
+  table.appendChild(tableValue);
+  document.getElementById("deptTable").appendChild(table);
+};
+
+//getLoan.addEventListener("click", handleGetLoan);
